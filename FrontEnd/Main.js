69273e5055b122fe265f45ec268cca16a79374d4
RyuSoul <quentin.barriere@gmail.com>
2025-04-22 17:47:24 +0200

    Première version modal galerie photo

--- FrontEnd/Main.js
+++ FrontEnd/Main.js
@@ -31,7 +31,8 @@ function card(imgUrl, title, categoryId) {
 const token = localStorage.getItem("token")
     console.log(token)
 const filtres = document.querySelector(".filtres")
-
+const portfolioModal = document.getElementById("portfolioModal")
+const feuilleModal = document.getElementById("feuilleModal")
     if (token) {
         const BtnConnexion = document.getElementById("login/out")
         const lienConnexion = document.getElementById("lienConnexion")
@@ -71,7 +72,54 @@ const filtres = document.querySelector(".filtres")
         icone2.classList.add("fa-solid" , "fa-pen-to-square");
         icone2.style.Color = "white";
         BtnModifié.prepend(icone2)
-    
+
+        BtnModifié.addEventListener("click", function(){
+            portfolioModal.setAttribute("aria-hidden", "false")
+            portfolioModal.style.display = "flex";
+            const Modalgallery = document.querySelector(".Modalgallery")
+
+            fetch("http://localhost:5678/api/works")
+            .then(response => response.json())
+            .then(data => {
+                data.forEach(element => {
+                    Modalcard(element.imageUrl, element.categoryId)
+                    console.log(element);
+                });
+            })
+            .catch(error => console.log(error));
+
+            function Modalcard(imgUrl, categoryId) {
+                const Modalfigure = document.createElement("Modalfigure");
+                const Modalimg = document.createElement("img");
+                const ModalTrash = document.createElement("i");
+                ModalTrash.classList.add("fa-solid", "fa-trash");
+                ModalTrash.classList.add("IconeTrash");
+                Modalimg.crossOrigin = "anonymous";
+                Modalimg.src = imgUrl;
+                Modalfigure.dataset.category = categoryId;
+                Modalfigure.classList.add("ModalFigure");
+                Modalfigure.appendChild(Modalimg);
+                Modalfigure.appendChild(ModalTrash);
+                Modalgallery.appendChild(Modalfigure);
+                }
+
+                const iconeCroix = document.createElement("i");
+                iconeCroix.classList.add("fa-solid", "fa-xmark");
+                feuilleModal.prepend(iconeCroix);
+                iconeCroix.classList.add("ModaliconeCroix")
+
+                const barreModal = document.createElement("span");
+                barreModal.classList.add("barreModal");
+                feuilleModal.appendChild(barreModal);
+
+                const BtnAjoutPhoto = document.createElement("button");
+                BtnAjoutPhoto.classList.add("photoBtn");
+                BtnAjoutPhoto.innerText = "Ajouter une photo";
+                feuilleModal.appendChild(BtnAjoutPhoto);
+            })
+
+            
+
     } else {
 
         /** Création des boutons pour filtrer*/
@@ -92,31 +140,31 @@ const filtres = document.querySelector(".filtres")
     })
     .catch(error => console.log(error));
 
-/** Fonction pour les filtres */    
-function filtre(id, name) {
-    /** Boutons pour filtrer*/ 
-    const Btnfiltre = document.createElement("button");
-    Btnfiltre.textContent = name;
-    Btnfiltre.dataset.category = id;
+    /** Fonction pour les filtres */    
+    function filtre(id, name) {
+        /** Boutons pour filtrer*/ 
+        const Btnfiltre = document.createElement("button");
+        Btnfiltre.textContent = name;
+        Btnfiltre.dataset.category = id;
 
-    filtres.appendChild(Btnfiltre);
-    filtres.classList.add("btn")
+        filtres.appendChild(Btnfiltre);
+        filtres.classList.add("btn")
 
-    Btnfiltre.addEventListener("click", function () {
-        const figures = document.querySelectorAll(".gallery figure");
-        const figuresArray = Array.from(figures); 
-        figuresArray.forEach(fig => {
-            fig.style.display = fig.dataset.category === Btnfiltre.dataset.category ? "block" : "none";
-        });
-    })
-    BtnTous.addEventListener("click", function () {
-        const figures = document.querySelectorAll(".gallery figure");
-        const figuresArray = Array.from(figures); 
-        figuresArray.forEach(fig => {
-            fig.style.display = "block"
-        });
-    })
-}
+        Btnfiltre.addEventListener("click", function () {
+            const figures = document.querySelectorAll(".gallery figure");
+            const figuresArray = Array.from(figures); 
+            figuresArray.forEach(fig => {
+                fig.style.display = fig.dataset.category === Btnfiltre.dataset.category ? "block" : "none";
+            });
+        })
+        BtnTous.addEventListener("click", function () {
+            const figures = document.querySelectorAll(".gallery figure");
+            const figuresArray = Array.from(figures); 
+            figuresArray.forEach(fig => {
+                fig.style.display = "block"
+            });
+        })
+    }
 }
 
 
